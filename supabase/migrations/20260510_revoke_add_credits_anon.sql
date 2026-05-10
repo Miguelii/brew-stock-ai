949358460bb974818f8e9022255e052aec9d075b
Miguel Gonçalves <miguelgoncalves18@hotmail.com>
2026-05-10 22:39:18 +0100

    supabase sql

--- supabase/migrations/20260510_revoke_add_credits_anon.sql
+++ supabase/migrations/20260510_revoke_add_credits_anon.sql
@@ -2,3 +2,6 @@ REVOKE EXECUTE ON FUNCTION public.add_credits(UUID, INTEGER) FROM anon;
 REVOKE EXECUTE ON FUNCTION public.add_credits(UUID, INTEGER) FROM authenticated;
 
 REVOKE EXECUTE ON FUNCTION public.deduct_credit(UUID, INTEGER) FROM anon;
+
+GRANT EXECUTE ON FUNCTION public.add_credits(UUID, INTEGER) TO service_role;
+GRANT EXECUTE ON FUNCTION public.add_credits(UUID, INTEGER) TO supabase_admin;

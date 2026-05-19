a73ec6caba1ed6bccdbe6f4865cd701cc28dfc9c
Miguel Gonçalves <miguelgoncalves18@hotmail.com>
2026-05-19 19:02:42 +0100

    feat: shouldCreateUser true

--- src/services/auth/sb-send-otp.ts
+++ src/services/auth/sb-send-otp.ts
@@ -13,7 +13,7 @@ export const sbSendOtp = Effect.fn('sbSendOtp')(function* (email: string) {
     })
 
     const { error } = yield* Effect.tryPromise({
-        try: () => supabase.auth.signInWithOtp({ email, options: { shouldCreateUser: false } }),
+        try: () => supabase.auth.signInWithOtp({ email, options: { shouldCreateUser: true } }),
         catch: (cause) => new SendOtpError({ cause, error_hash: ErrorCode.AUTH_OTP_SEND_REQUEST }),
     })
 

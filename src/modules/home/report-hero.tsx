dc91482566a3b9d584bac5c2f61cc2f793210d79
Miguel Gonçalves <miguelgoncalves18@hotmail.com>
2026-07-23 15:34:54 +0100

    change: remove padding

--- src/modules/home/report-hero.tsx
+++ src/modules/home/report-hero.tsx
@@ -12,7 +12,7 @@ const stats = [
 export function ReportHero() {
     return (
         <section className="main-container-lg border-b border-border">
-            <div className="px-6 pb-24 lg:pb-32">
+            <div className="pb-24 lg:pb-32">
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 items-center">
                     <motion.div
                         initial={{ opacity: 0, y: 28 }}
